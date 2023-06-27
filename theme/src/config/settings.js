65d5c03799e252aae9f09339c9b458d44d92fc9e
Alvaro Fragoso <alvarofragosoc@hotmail.com>
2023-06-27 14:28:26 +0000

    some changes

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `4mentor | Real Estate`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://4mentor.com`, // No trailing slash!
-	name: `4mentor`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `mistral | Real Estate`,
+	description: `Your real estate partner.`,
+	url: `https://mistralhomes.com`, // No trailing slash!
+	name: `mistral`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
